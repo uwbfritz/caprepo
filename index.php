f9d5deba54c9127fa92e246d9b8250951a0e738d
root <root@control.uwtci.com>
2023-02-09 11:59:39 -0800

    09/02/2023_11:59:39 Working in: /tmp/caprover/php Committed by: root

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Bill Test....</title>
+  <title>Bill Testing</title>
 
   <!-- Bootstrap Core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
